b8ce3cd0cc68161f80e36cb5c17f250fccaf6f18
misaina08 <misa.andrianary@gmail.com>
2018-01-16 22:21:49 +0300

    gestion intervenants dossier

--- SmartLaw/SmartLaw-war/src/java/managedbean/dossier/FicheDossierMB.java
+++ SmartLaw/SmartLaw-war/src/java/managedbean/dossier/FicheDossierMB.java
@@ -15,6 +15,7 @@ import modeles.dossiers.ContactDossierLibelle;
 import modeles.dossiers.DossierLibelle;
 import modeles.evenement.EvtDossierLibelle;
 import modeles.intervenants.IntervDossierLibelle;
+import modeles.intervenants.Intervenant;
 import modeles.intervenants.IntervenantDossier;
 import utilitaire.MessageUtil;
 
@@ -25,22 +26,24 @@ import utilitaire.MessageUtil;
 @Named(value = "ficheDossierMB")
 @ViewScoped
 public class FicheDossierMB implements Serializable {
-    
+
     @EJB
     private GeneriqueBean generiqueBean;
-    
+
     private Integer idDossier;
     private DossierLibelle dossier;
     private List<IntervDossierLibelle> intervs;
     private ContactDossierLibelle contDoss;
     private List<EvtDossierLibelle> listeEvt;
+    private List<Intervenant> intervenants;
+    private Integer idIntervSelected;
 
     /**
      * Creates a new instance of FicheDossierMB
      */
     public FicheDossierMB() {
     }
-    
+
     public void loadDossier() {
         try {
             DossierLibelle d = new DossierLibelle();
@@ -51,35 +54,80 @@ public class FicheDossierMB implements Serializable {
             MessageUtil.addFlashErrorMessage("Erreur de chargement");
         }
     }
-    
+
     public void supprimerIntervenant(Integer id) {
         try {
-            IntervenantDossier interv = new IntervenantDossier();
-            interv.setId(id);
-            generiqueBean.getService().delete(interv);
+            IntervenantDossier intervDel = new IntervenantDossier();
+            intervDel.setId(id);
+            generiqueBean.getService().delete(intervDel);
+
+            IntervDossierLibelle i = new IntervDossierLibelle();
+            i.setIdDossier(idDossier);
+            intervs = (List<IntervDossierLibelle>) (List<?>) generiqueBean.getService().find(i);
+
             MessageUtil.messageInfo("Intervenant supprimé");
         } catch (Exception ex) {
             ex.printStackTrace();
             MessageUtil.messageErreur("Erreur de suppression");
         }
     }
-    
+
+    public void loadAddIntervenants() {
+        try {
+            System.out.println("debut");
+            if (intervenants == null) {
+                System.out.println("null");
+                intervenants = (List<Intervenant>) (List<?>) generiqueBean.getService().find(new Intervenant());
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void ajouterIntervenant() {
+        try {
+            IntervenantDossier intervDoss = new IntervenantDossier();
+            intervDoss.setIdDossier(idDossier);
+            intervDoss.setIdIntervenant(idIntervSelected);
+            int mtovy = 0;
+            for (IntervDossierLibelle idl : getIntervs()) {
+                if (idl.getIdIntervenant().equals(intervDoss.getIdIntervenant())) {
+                    mtovy++;
+                    break;
+                }
+            }
+            if (mtovy == 0) {
+                IntervenantDossier ido = new IntervenantDossier();
+                ido.setIdDossier(idDossier);
+                ido.setIdIntervenant(idIntervSelected);
+                generiqueBean.getService().save(ido);
+                IntervDossierLibelle i = new IntervDossierLibelle();
+                i.setIdDossier(idDossier);
+                intervs = (List<IntervDossierLibelle>) (List<?>) generiqueBean.getService().find(i);
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+        }
+    }
+
     public Integer getIdDossier() {
         return idDossier;
     }
-    
+
     public void setIdDossier(Integer idDossier) {
         this.idDossier = idDossier;
     }
-    
+
     public DossierLibelle getDossier() {
         return dossier;
     }
-    
+
     public void setDossier(DossierLibelle dossier) {
         this.dossier = dossier;
     }
-    
+
     public List<IntervDossierLibelle> getIntervs() {
         if (intervs == null) {
             try {
@@ -92,11 +140,11 @@ public class FicheDossierMB implements Serializable {
         }
         return intervs;
     }
-    
+
     public void setIntervs(List<IntervDossierLibelle> intervs) {
         this.intervs = intervs;
     }
-    
+
     public ContactDossierLibelle getContDoss() {
         if (contDoss == null) {
             try {
@@ -110,11 +158,11 @@ public class FicheDossierMB implements Serializable {
         }
         return contDoss;
     }
-    
+
     public void setContDoss(ContactDossierLibelle contDoss) {
         this.contDoss = contDoss;
     }
-    
+
     public List<EvtDossierLibelle> getListeEvt() {
         if (listeEvt == null) {
             try {
@@ -128,9 +176,25 @@ public class FicheDossierMB implements Serializable {
         }
         return listeEvt;
     }
-    
+
     public void setListeEvt(List<EvtDossierLibelle> listeEvt) {
         this.listeEvt = listeEvt;
     }
-    
+
+    public List<Intervenant> getIntervenants() {
+        return intervenants;
+    }
+
+    public void setIntervenants(List<Intervenant> intervenant) {
+        this.intervenants = intervenant;
+    }
+
+    public Integer getIdIntervSelected() {
+        return idIntervSelected;
+    }
+
+    public void setIdIntervSelected(Integer idIntervSelected) {
+        this.idIntervSelected = idIntervSelected;
+    }
+
 }
